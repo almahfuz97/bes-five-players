1969eb9e85b136715ce0f0a44c36a97c9983f71b
almahfuz <almahfuz0327@gmail.com>
2022-08-22 20:32:55 +0600

    calculated budget, expenses with js

--- script/main.js
+++ script/main.js
@@ -1,28 +1,72 @@
+// function for getting input value
+function getValueFromInput(inputId) {
+    const inputField = document.getElementById(inputId);
+    const fieldValue = inputField.value;
+    if (fieldValue !== '') {
+        return fieldValue;
+    }
+    else {
+        alert('please provide a valid number!')
+        inputField.value = '';
+        return '';
+    }
+}
+
+// selecting players-section
 const ulList = document.getElementById('select-list-id');
-
-const selects = document.getElementsByClassName('select-btn');
 let listLength = ulList.children.length;
-
+const selects = document.getElementsByClassName('select-btn');
+// getting select buttons,and implementing click eventListener
 for (const select of selects) {
-
     select.addEventListener('click', function (event) {
         // console.log(listLength)
         if (listLength < 5) {
             // creating list element
             const li = document.createElement('li');
-            // finding players name and setting it to li
+            // finding targeted players name and setting it to li
             li.innerText = select.previousSibling.previousSibling.previousSibling.previousSibling.innerText;
             li.style.color = 'white'
-            li.style.marginBottom = '20px'
+            li.style.marginBottom = '16px'
             select.setAttribute('disabled', 'disabled');
             select.style.backgroundColor = 'gray';
             ulList.appendChild(li);
             listLength = ulList.children.length;
-            console.log(listLength);
         }
         else {
             alert('You have already selected 5 best Players!')
         }
     })
-
 }
+
+// calculate Button
+const playersExpenses = document.getElementById('players-expenses');
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    // calling function to get inputvalue
+    const inputValue = getValueFromInput('per-player');
+
+    if (inputValue !== '') {
+        playersExpenses.innerText = listLength * parseFloat(inputValue);
+    }
+    else {
+        playersExpenses.innerText = '0';
+
+    }
+})
+
+// calculate total button
+document.getElementById('calculate-total-btn').addEventListener('click', function () {
+    // calling function to get input value
+    const inputValueManger = getValueFromInput('manager-input');
+    const inputValueCoach = getValueFromInput('coach-input');
+
+    if (inputValueManger !== '' && inputValueCoach !== '') {
+        const total = document.getElementById('total');
+        total.innerText = parseFloat(inputValueManger) + parseFloat(inputValueCoach) + parseFloat(playersExpenses.innerText);
+    }
+    else {
+        total.innerText = '0';
+
+    }
+})
+
+
